a6135c7460e94d3dc3a66df4e17df386645c64d1
Steven Haryanto (on Asus EEEPC) <stevenharyanto@gmail.com>
2012-11-18 13:28:43 +0700

    Add FAQ entries (responding to recent CPAN ratings by zaxon)

--- lib/HTTP/Headers/Patch/DontUseStorable.pm
+++ lib/HTTP/Headers/Patch/DontUseStorable.pm
@@ -55,6 +55,18 @@ This patch avoids using Storable and clone using the alternative method.
 
 =head1 FAQ
 
+=head2 Is this a bug with HTTP::Headers? Why don't you submit a bug to HTTP-Message?
+
+I tend not to think this is a bug with HTTP::Headers; after all, Storable's
+dclone() is a pretty standard way to clone data in Perl. This patch is more of a
+workaround for current Storable's deficiencies.
+
+=head2 Shouldn't you add STORABLE_{freeze,thaw} methods to Regexp instead?
+
+Yes, that's also one possible solution, albeit with slightly more work. There's
+an old distribution, L<Regexp::Copy> (last release is 0.06 in 2003, at the time
+of this writing) which does this, but last time I tried it no longer works.
+
 
 =head1 SEE ALSO
 
